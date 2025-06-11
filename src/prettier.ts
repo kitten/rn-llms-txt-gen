5ab3f568c6f5670145c298f36d28bb7cc04be086
Phil Pluckthun <phil@kitten.sh>
2025-06-11 23:52:47 +0100

    Reformat prettier to get rid of table spacing

--- src/prettier.ts
+++ src/prettier.ts
@@ -1,11 +1,34 @@
 import { format } from 'prettier';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkStringify from 'remark-stringify';
+import remarkGfm from 'remark-gfm';
+
+async function reformat(content: string): Promise<string> {
+  // NOTE: We reformat with remark again to get rid of prettier's
+  // table formatting mostly. This doesn't work well as LLM input
+  const md = await unified()
+    .use(remarkParse, { fragment: true })
+    .use(remarkGfm, {
+      tablePipeAlign: false,
+      tableCellPadding: false,
+    })
+    .use(remarkStringify, {
+      bullet: '-',
+      incrementListMarker: false,
+      ruleSpaces: false,
+      tightDefinitions: true,
+    })
+    .process(content);
+  return md.toString();
+}
 
 export async function formatMarkdown(input: string) {
-  return format(input, {
+  return reformat(await format(input, {
     semi: false,
     singleQuote: false,
     trailingComma: 'es5',
     proseWrap: 'never',
     parser: 'markdown',
-  });
+  }));
 }
